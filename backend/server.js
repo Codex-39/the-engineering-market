e8ec6c81826f54c51bb89c16f03cb42bed00fdfb
Raza <ahamedraza0039@gmail.com>
2026-06-11 11:55:21 +0530

    Allow Vercel frontend in CORS

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,7 @@ app.use(cors({
     'http://localhost:5174',
     'http://127.0.0.1:5173',
     'http://127.0.0.1:5174',
+    'https://the-engineering-market.vercel.app'
   ],
   credentials: true,
 }));
